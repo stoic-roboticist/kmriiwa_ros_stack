364caea888d36560328580d2215b10a7a5eb6efb
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2021-03-19 09:31:02 +0000

    fixed catkin package build files and prep for travis ci

---
+++ .travis/ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get -qq update
+apt-get -qq dist-upgrade
+rosdep update
+apt-get -qq install git
+apt-get -qq install python3-vcstool
+vcs import < .travis/dep.repos
+cd /root/noetic_ws
+rosdep install --from-paths src --ignore-src -r -y > /dev/null
+catkin_make_isolated --quiet
